b9597bb667a71bf630301231faee12cf49c517e7
faridbandiyev <bendiyevf@gmail.com>
2024-05-26 13:46:14 +0400

    Notes

--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -12,14 +12,63 @@ namespace WinFormsApp1
 {
     public partial class NoteTaker : Form
     {
+        DataTable notes = new DataTable();
+        bool editing = false;
         public NoteTaker()
         {
             InitializeComponent();
+            Load += NoteTaker_Load;
         }
 
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                notes.Rows[previousNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception ex) { Console.WriteLine("Not a valid note"); }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+        }
+        private void newNoteButton_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = "";
+            noteBox.Text = "";
+        }
         private void NoteTaker_Load(object sender, EventArgs e)
         {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
 
+            previousNotes.DataSource = notes;
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (editing)
+            {
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Title"] = titleBox.Text;
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Note"] = noteBox.Text;
+
+            }
+            else
+            {
+                notes.Rows.Add(titleBox.Text, noteBox.Text);
+            }
+            titleBox.Text = "";
+            noteBox.Text = "";
+            editing = false;
+        }
+
+        private void previousNotes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            titleBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
         }
     }
 }
